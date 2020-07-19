f0bde969160318dfe607d05fad0fffc041768cac
Muntaser Syed <jemsbhai@gmail.com>
2020-07-19 08:24:28 -0400

    Create getfishyreviews.py

---
+++ server/getfishyreviews.py
@@ -0,0 +1,71 @@
+import os
+import pymongo
+import json
+
+def dummy(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    if request.method == 'OPTIONS':
+        # Allows GET requests from origin https://mydomain.com with
+        # Authorization header
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Max-Age': '3600',
+            'Access-Control-Allow-Credentials': 'true'
+        }
+        return ('', 204, headers)
+
+    # Set CORS headers for main requests
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': 'true'
+    }
+
+    request_json = request.get_json()
+    mongostr = os.environ.get('MONGOSTR')
+    client = pymongo.MongoClient(mongostr)
+    db = client["herefishyfishy"]
+    col = db.reviews
+    results = []
+    maxid = 0
+    dishname = request_json['reviews']
+    
+
+    for x in col.find():
+        item = {}
+        if str(x['dish']) == str(dishname):
+            item["dish"] =  str(x["dish"])
+            item["review"] =  x["review"]
+            item["name"] =  x["name"]
+            # item["status"] =  x["status"]
+            # item["description"] =  x["description"]
+            
+            
+            results.append(item)
+        maxid +=1
+    
+    
+    retjson = {}
+
+    retjson['reviews'] = results
+    retjson['mongoresult'] = str(maxid)
+
+    return json.dumps(retjson)
+
+
+    retstr = "action not done"
+
+    if request.args and 'message' in request.args:
+        return request.args.get('message')
+    elif request_json and 'message' in request_json:
+        return request_json['message']
+    else:
+        return retstr
